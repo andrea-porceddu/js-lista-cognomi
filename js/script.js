913e142c2a0c63fd1ffa5c366b8e8eb074ecf300
andrea <breezer087@gmail.com>
2020-12-16 16:38:06 +0100

    trying to lowercase each stringArray element and put to another array than sort()

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var array = [
+var stringArray = [
   'Pippo',
   'Franco',
   'Franchi',
@@ -11,13 +11,23 @@ var array = [
   'Rosa',
 ];
 
-// works with uppercase first character
-var input = prompt('Inserisci qualcosa');
-array.push(input);
+// works with uppercase first character and strings - no numbers
+// var input = prompt('Inserisci qualcosa');
 // console.log(array);
-if (input) {
-  array.sort();
-}
-console.log(array);
+// if (input) {
+//   stringArray.push(input);
+//   stringArray.sort();
+// }
+// console.log(stringArray);
 
-// lowercase first character
+// lowercase first character - toUpperCase()/toLowerCase() methods does not change the original string
+// trying to put each stringArray elements, and transform it to lowercase, inside another Array and after sort() it
+var input = prompt('Inserisci qualcosa');
+stringArray.push(input);
+// stringArray lowercase transform
+var lowerCaseArray = [];
+for (i = 0; i < stringArray.length; i++) {
+  lowerCaseArray.push(stringArray[i].toLowerCase());
+};
+lowerCaseArray.sort();
+console.log(lowerCaseArray);
